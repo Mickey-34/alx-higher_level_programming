d31f6b3075bae6c865b75bb2f1fb14d95f4a92ae
Mickey-34 <abbeyella54@gmail.com>
2023-11-03 21:19:58 -0700

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    comment
+'''
+import urllib
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as status:
+                print(status.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
