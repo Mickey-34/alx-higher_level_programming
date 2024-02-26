78f8cfc7ac8aab8ec6f5a003411c01b3fb4d0edc
Mickey-34 <128931001+Mickey-34@users.noreply.github.com>
2024-02-26 18:07:50 +0000

    Delete 0x11-python-network_1 directory
    delete

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''
-    comment
-'''
-import requests
-
-r = requests.get('https://intranet.hbtn.io/status')
-print('Body response:')
-print('\t- type: {}\n\t- content: {}'.format(type(r.text), r.text))
