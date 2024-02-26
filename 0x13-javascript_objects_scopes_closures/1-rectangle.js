f1b4839767feffb827e99a226278b45adcab4ac0
Mickey-34 <128931001+Mickey-34@users.noreply.github.com>
2024-02-26 17:59:09 +0000

    Delete 0x13-javascript_objects_scopes_closures directory
    delete

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (o, y) {
-    this.width = o;
-    this.height = y;
-  }
-};
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-// Import an array and compute a new array
-const oldlist = require('./100-data').list;
-const newlist = oldlist.map((v, m) => v * m);
-console.log(oldlist);
-console.log(newlist);
